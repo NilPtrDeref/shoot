40d964537de006e66e7f2305f6941320482c511b
james.wood <jwooddev@gmail.com>
2024-07-25 11:02:03 -0500

    implement movement

--- game/game.go
+++ game/game.go
@@ -1,7 +1,9 @@
 package game
 
 import (
+	"encoding/json"
 	"errors"
+	"slices"
 	"sync"
 	"time"
 
@@ -14,6 +16,9 @@ import (
 // TODO: On player add, send them their id
 
 const (
+	Width         = 768
+	Height        = 768
+	MoveDelta     = 5
 	RefreshRate   = time.Second / 1
 	PingTime      = 10 * time.Second
 	PongTimeout   = 12 * time.Second
@@ -48,9 +53,22 @@ func (g *Game) GetRoom(id string) *Room {
 	return nil
 }
 
+type Movement struct {
+	Up    bool `json:"up"`
+	Down  bool `json:"down"`
+	Left  bool `json:"left"`
+	Right bool `json:"right"`
+}
+
+type Event struct {
+	Type      string `json:"type"`
+	Sequence  int64  `json:"sequence"`
+	*Movement `json:"movement"`
+}
+
 type Message struct {
-	ID      string
-	Message []byte
+	ID    string
+	Event Event
 }
 
 type Room struct {
@@ -59,6 +77,7 @@ type Room struct {
 	Slots       int64  `json:"slots"`
 	players     []Player
 	PlayerCount int64 `json:"player_count"`
+	updates     bool
 	receive     chan Message
 	*sync.RWMutex
 }
@@ -70,7 +89,6 @@ func (r *Room) PlayLoop() {
 	for {
 		select {
 		// TODO: Handle these messages
-		// Joins
 		// Movement
 		// Shoot
 		// Respawn
@@ -82,14 +100,48 @@ func (r *Room) PlayLoop() {
 
 			logrus.WithFields(
 				logrus.Fields{
-					"msg":    string(message.Message),
+					"event":  message.Event,
 					"player": message.ID,
 				},
-			).Info("received message")
+			).Debug("received message")
 
-		// TODO: Broadcast out the game state
+			switch message.Event.Type {
+			case "movement":
+				r.RLock()
+				player := r.FindPlayer(message.ID)
+				if player != nil {
+					player.Move(message.Event.Sequence, message.Event.Movement)
+				}
+				r.RUnlock()
+			}
+
 		case <-ticker.C:
-			logrus.Info("updating clients")
+			r.Lock()
+			if !r.updates {
+				r.Unlock()
+				continue
+			}
+			logrus.Debug("updating clients")
+
+			msg, err := json.Marshal(map[string]any{
+				"type":    "update",
+				"players": &r.players,
+			})
+			if err != nil {
+				r.Unlock()
+				logrus.WithFields(
+					logrus.Fields{
+						"room": r.ID,
+					},
+				).WithError(err).Error("failed to marshal game state")
+				break
+			}
+
+			for _, player := range r.players {
+				player.send <- msg
+			}
+			r.updates = false
+			r.Unlock()
 		}
 	}
 }
@@ -102,9 +154,25 @@ func (r *Room) AddPlayer(conn *websocket.Conn) error {
 		return errors.New("room is full")
 	}
 
-	player := Player{ID: uuid.New().String(), conn: conn, room: r, send: make(chan []byte, 10), close: make(chan struct{})}
+	player := Player{
+		ID: uuid.New().String(),
+		Info: PlayerInfo{
+			X:      0,
+			Y:      0,
+			radius: 10,
+		},
+		conn:  conn,
+		room:  r,
+		send:  make(chan []byte, 10),
+		close: make(chan struct{}),
+	}
 	r.players = append(r.players, player)
 	r.PlayerCount = int64(len(r.players))
+	player.conn.WriteJSON(map[string]any{
+		"type":    "bootstrap",
+		"id":      player.ID,
+		"players": &r.players,
+	})
 	go player.Handle()
 	return nil
 }
@@ -123,13 +191,30 @@ func (r *Room) RemovePlayer(player *Player) {
 	}
 }
 
-type Player struct {
-	ID       string `json:"id"`
-	conn     *websocket.Conn
+func (r *Room) FindPlayer(id string) *Player {
+	index := slices.IndexFunc(r.players, func(player Player) bool {
+		return player.ID == id
+	})
+	if index < 0 {
+		return nil
+	}
+	return &r.players[index]
+}
+
+type PlayerInfo struct {
 	Sequence int64 `json:"sequence"`
-	room     *Room
-	send     chan []byte
-	close    chan struct{}
+	X        int64 `json:"x"`
+	Y        int64 `json:"y"`
+	radius   int64
+}
+
+type Player struct {
+	ID    string `json:"id"`
+	conn  *websocket.Conn
+	Info  PlayerInfo `json:"info"`
+	room  *Room
+	send  chan []byte
+	close chan struct{}
 }
 
 func (p *Player) Handle() {
@@ -173,12 +258,24 @@ func (p *Player) HandleReads() {
 			break
 		}
 
-		p.room.receive <- Message{p.ID, message}
+		var event Event
+		err = json.Unmarshal(message, &event)
+		if err != nil {
+			logrus.WithFields(
+				logrus.Fields{
+					"id":   p.ID,
+					"room": p.room.ID,
+				},
+			).WithError(err).Error("unexpected message format")
+			break
+		}
+
+		p.room.receive <- Message{p.ID, event}
 		logrus.WithFields(logrus.Fields{
 			"id":   p.ID,
 			"room": p.room.ID,
 			"msg":  string(message),
-		}).Infof("received message")
+		}).Debugf("received message")
 	}
 }
 
@@ -217,4 +314,39 @@ func (p *Player) HandleWrites() {
 			return
 		}
 	}
+}
+
+func (p *Player) Move(sequence int64, movement *Movement) {
+	p.Info.Sequence = sequence
+
+	if movement.Up {
+		if p.Info.Y >= (p.Info.radius + MoveDelta) {
+			p.Info.Y -= MoveDelta
+		} else {
+			p.Info.Y = 0 + p.Info.radius
+		}
+	}
+	if movement.Down {
+		if p.Info.Y <= Height-(p.Info.radius+MoveDelta) {
+			p.Info.Y += MoveDelta
+		} else {
+			p.Info.Y = Height - p.Info.radius
+		}
+	}
+	if movement.Left {
+		if p.Info.X >= (p.Info.radius + MoveDelta) {
+			p.Info.X -= MoveDelta
+		} else {
+			p.Info.X = 0 + p.Info.radius
+		}
+	}
+	if movement.Right {
+		if p.Info.X <= Width-(p.Info.radius+MoveDelta) {
+			p.Info.X += MoveDelta
+		} else {
+			p.Info.X = Width - p.Info.radius
+		}
+	}
+
+	p.room.updates = true
 }
--- public/game.js
+++ public/game.js
@@ -5,6 +5,8 @@ class Game extends HTMLElement {
 
     this.handle_keypress = this.handle_keypress.bind(this);
     this.handle_message = this.handle_message.bind(this);
+    this.animate = this.animate.bind(this);
+    this.sequence = 1;
   }
 
   handle_keypress(event) {
@@ -27,12 +29,13 @@ class Game extends HTMLElement {
   handle_message(message) {
     let data = JSON.parse(message.data);
 
-    if (data.type === "join") {
-      console.log(data);
+    if (data.type === "bootstrap") {
+      this.self = data.id;
+      this.players = data.players;
     }
 
     if (data.type === "update") {
-      console.log(data);
+      this.players = data.players;
     }
 
     // TODO: Handle errors appropriately
@@ -40,6 +43,46 @@ class Game extends HTMLElement {
       console.log(data);
       this.ws.close();
     }
+  }
+
+  animate() {
+    // TODO: Slow this down, sends too frequently
+    if (
+      this.movement.up ||
+      this.movement.down ||
+      this.movement.left ||
+      this.movement.right
+    ) {
+      this.ws.send(
+        JSON.stringify({
+          type: "movement",
+          sequence: this.sequence,
+          movement: this.movement,
+        }),
+      );
+      this.sequence++;
+    }
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    if (this.players) {
+      this.players.forEach((player) => {
+        this.ctx.beginPath();
+        this.ctx.arc(
+          player.info.x * this.dpr,
+          player.info.y * this.dpr,
+          10 * this.dpr,
+          0,
+          Math.PI * 2,
+          false,
+        );
+        this.ctx.fillStyle = "black";
+        this.ctx.fill();
+        this.ctx.closePath();
+      });
+    }
+
+    requestAnimationFrame(this.animate);
   }
 
   connectedCallback() {
@@ -50,7 +93,16 @@ class Game extends HTMLElement {
       <canvas id="game-window"></canvas>
     `;
 
-    this.canvas = this.shadowRoot.querySelector("#game-window");
+    this.dpr = window.devicePixelRatio ?? 1;
+    this.canvas = this.shadowRoot.getElementById("game-window");
+    // TODO: Handle error when canvas can't be found
+    if (!this.canvas) {
+      console.error("failed to load canvas");
+      return;
+    }
+    this.canvas.width = 768 * this.dpr;
+    this.canvas.height = 768 * this.dpr;
+
     this.ctx = this.canvas.getContext("2d");
     this.room = this.getAttribute("room");
     this.ws = new WebSocket(`/room/${this.room}/ws`);
@@ -64,6 +116,7 @@ class Game extends HTMLElement {
 
     window.addEventListener("keydown", this.handle_keypress);
     window.addEventListener("keyup", this.handle_keypress);
+    this.animate();
   }
 
   disconnectedCallback() {
